f823bf8fecbda5cfcb6ef207d17d0da9f1df2034
Pablo Navarro Castillo <pnavarrc@gmail.com>
2017-04-25 08:31:35 -0700

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # zen-repl
 Configurable JavaScript REPL
+
+### Install
+
+```
+$ npm install --global zen-repl
+```
+
+### Usage
+
+```
+$ zen-repl
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+'use strict';
+
+const history = require('repl.history');
+const repl    = require('repl');
+const R       = require('ramda');
+
+const addReadOnlyGlobal = (context, name, value) => {
+  Object.defineProperty(context, name, {
+    configurable: false,
+    enumerable:   true,
+    value:        value
+  })
+};
+
+const config = {
+  prompt:    '𝛌 ',
+  replMode:  repl.REPL_MODE_STRICT,
+  useColors: true
+};
+
+const r = repl.start(config);
+addReadOnlyGlobal(r.context, 'R', R);
+
+replHistory(r, process.env.HOME + '/.zen-repl-history');
